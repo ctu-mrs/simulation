b451e12c8c30636c3f376e0764b204a9cb360811
Tomas Baca <klaxalk@gmail.com>
2020-05-04 20:02:34 +0200

    + install scripts

---
+++ installation/install.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# get the path to this script
+MY_PATH=`dirname "$0"`
+MY_PATH=`( cd "$MY_PATH" && pwd )`
+
+set -e
+
+trap 'last_command=$current_command; current_command=$BASH_COMMAND; echo running "$current_command"' DEBUG
+trap 'echo "\"${last_command}\" command failed with exit code $?"' EXIT
+
+# install git
+sudo apt-get -y install git
+
+# install gitman
+
+unattended=0
+subinstall_params=""
+for param in "$@"
+do
+  echo $param
+  if [ $param="--unattended" ]; then
+    echo "installing in unattended mode"
+    unattended=1
+    subinstall_params="--unattended"
+  fi
+done
+
+## | ----------------------- Install ROS ---------------------- |
+
+default=y
+while true; do
+  if [[ "$unattended" == "1" ]]
+  then
+    resp=$default
+  else
+    [[ -t 0 ]] && { read -t 10 -n 2 -p $'\e[1;32mInstall ROS? [y/n] (default: '"$default"$')\e[0m\n' resp || resp=$default ; }
+  fi
+  response=`echo $resp | sed -r 's/(.*)$/\1=/'`
+
+  if [[ $response =~ ^(y|Y)=$ ]]
+  then
+
+    $MY_PATH/dependencies/ros.sh
+
+    break
+  elif [[ $response =~ ^(n|N)=$ ]]
+  then
+    break
+  else
+    echo " What? \"$resp\" is not a correct answer. Try y+Enter."
+  fi
+done
+
+## | --------------------- Install Gitman --------------------- |
+$MY_PATH/dependencies/gitman.sh
+
+## | --------------------- Install git lfs --------------------- |
+$MY_PATH/dependencies/git_lfs.sh
