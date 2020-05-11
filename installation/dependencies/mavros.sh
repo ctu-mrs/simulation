06f8bef3926b19a65b97d5552932d19623551141
Tomas Baca <klaxalk@gmail.com>
2020-05-11 13:51:00 +0200

    added missing mavros extras dependency

--- installation/dependencies/mavros.sh
+++ installation/dependencies/mavros.sh
@@ -7,7 +7,7 @@ trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
 
 echo "Installing Mavros"
 
-sudo apt -y install ros-melodic-mavros
+sudo apt -y install ros-melodic-mavros ros-melodic-mavros-extras
 
 cd /opt/ros/melodic/lib/mavros
 sudo ./install_geographiclib_datasets.sh
