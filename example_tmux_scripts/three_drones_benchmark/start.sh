31ce620b1a96abba8091017be10b3f42b713a7c3
klaxalk <klaxalk@gmail.com>
2020-09-15 11:57:44 +0200

    updated start.sh scripts to allow running from other location

--- example_tmux_scripts/three_drones_benchmark/start.sh
+++ example_tmux_scripts/three_drones_benchmark/start.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-if_is_link=`readlink $0`
-command_name=$0
-if [[ if_is_link ]]; then
-  command_name=$if_is_link
-fi
-
-MY_PATH=`dirname "$if_is_link"`
-MY_PATH=`( cd "$MY_PATH" && pwd )`
-cd $MY_PATH
+# Absolute path to this script. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f $0)
+# Absolute path this script is in. /home/user/bin
+SCRIPTPATH=`dirname $SCRIPT`
+cd "$SCRIPTPATH"
 
 # remove the old link
 rm .tmuxinator.yml
