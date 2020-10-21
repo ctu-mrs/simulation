37ed9fba73e5fcc51b76aae14b732b8b0ba6f401
klaxalk <klaxalk@gmail.com>
2020-10-21 14:49:56 +0200

    reverted travis to master

--- .ci/ci_before_install.sh
+++ .ci/ci_before_install.sh
@@ -30,14 +30,12 @@ echo "installing uav_core"
 cd
 git clone https://github.com/ctu-mrs/uav_core
 cd uav_core
-git checkout new_firmware
 ./installation/install.sh
 
 echo "installing simulation"
 cd
 git clone https://github.com/ctu-mrs/simulation
 cd simulation
-git checkout new_firmware
 ./installation/install.sh
 
 echo "creating workspace"
