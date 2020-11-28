fa86cf125781b70780d90b6984f7c65b568bde9e
Tomas Baca <klaxalk@gmail.com>
2020-11-28 15:33:43 +0100

    travis -> actions

--- .ci/install.sh
+++ .ci/install.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
-# author: Robert Penicka
-
 set -e
 
 distro=`lsb_release -r | awk '{ print $2 }'`
 [ "$distro" = "18.04" ] && ROS_DISTRO="melodic"
 [ "$distro" = "20.04" ] && ROS_DISTRO="noetic"
 
-echo "Starting install preparation"
-openssl aes-256-cbc -K $encrypted_f0fd3ee254e8_key -iv $encrypted_f0fd3ee254e8_iv -in ./.ci/deploy_key_github.enc -out ./.ci/deploy_key_github -d
-eval "$(ssh-agent -s)"
-chmod 600 ./.ci/deploy_key_github
-ssh-add ./.ci/deploy_key_github
+echo "Starting install" 
 
 sudo apt-get -y update -qq
-
 sudo apt-mark hold openssh-server
+
+# 20.04 problem fix
+sudo apt-get -y install grub-efi
+sudo update-grub
 
 # the "gce-compute-image-packages" package often freezes the installation at some point
 # the installation freezes when it tries to manage some systemd services
@@ -24,7 +21,7 @@ sudo apt-mark hold openssh-server
 
 sudo apt-get -y upgrade --fix-missing
 
-sudo apt-get -y install git # python-setuptools python3-setuptools python3-pip
+sudo apt-get -y install dpkg git
 
 echo "installing uav_core"
 cd
