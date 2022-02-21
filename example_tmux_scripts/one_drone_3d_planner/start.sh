072ace603a2312609dcca1771b539789e566fb83
Dan Hert <hertdani@fel.cvut.cz>
2022-02-21 15:08:31 +0100

    added 3D planner example tmux

---
+++ example_tmux_scripts/one_drone_3d_planner/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Absolute path to this script. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f $0)
+# Absolute path this script is in. /home/user/bin
+SCRIPTPATH=`dirname $SCRIPT`
+cd "$SCRIPTPATH"
+
+# start tmuxinator
+tmuxinator start -p ./session.yml
+
