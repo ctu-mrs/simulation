20f23ce518dcfa7b86e6f4db1fa6df12b41ee2ea
Tomas Baca <klaxalk@gmail.com>
2022-03-06 20:14:11 +0200

    added aloam example session

---
+++ example_tmux_scripts/one_drone_aloam_slam/start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Absolute path to this script. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f $0)
+# Absolute path this script is in. /home/user/bin
+SCRIPTPATH=`dirname $SCRIPT`
+cd "$SCRIPTPATH"
+
+rosrun mrs_uav_general check_ros_package.sh ./required_packages.txt
+
+if [[ $? -eq 0 ]]; then
+  # start tmuxinator
+  tmuxinator start -p ./session.yml
+fi
