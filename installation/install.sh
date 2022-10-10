06867fd065c78a76b857b749fc61026844dcef57
Tomas Baca <klaxalk@gmail.com>
2022-10-10 16:56:52 +0200

    disabled upgrade of ignition

--- installation/install.sh
+++ installation/install.sh
@@ -52,8 +52,9 @@ fi
 [ $DEBIAN ] && $MY_PATH/../ros_packages/px4_firmware/Tools/setup/debian.sh --no-nuttx --no-sim-tool
 
 sudo apt-get -y update
-#to fix mrs_gazebo_common_resources build on Ubuntu 20.04
-sudo apt-get upgrade -y libignition-common3*
+
+# to fix mrs_gazebo_common_resources build on Ubuntu 20.04
+# sudo apt-get upgrade -y libignition-common3*
 
 if [ "$distro" = "18.04" ]; then
   sudo -H pip install --user packaging toml
